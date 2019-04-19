483f97f142a5579a3a7384c738dfb9fe306776ff
addy2053 <32369172+addy2053@users.noreply.github.com>
2019-04-20 01:17:30 +0500

    backup

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+
+engine = create_engine(os.getenv("postgres://jypthddpgzkyyv:41eaa68d8f4d665a3d27f4c5d7f1d1f3ff80d9652de2f52b35b0c28a788c9662@ec2-54-83-205-27.compute-1.amazonaws.com:5432/d8dd3keevdr4gq"))
+
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
 def index():
@@ -11,6 +19,8 @@ ab=[]
 def a():
     # return 'Hello World!'
     # if request.method == "get":
+
+    #     aaa = db.execute().fetchall()
         n=request.form.get("name")
         # print(n)/
         ab.append(n)
